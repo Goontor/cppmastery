416ccd08de765441a293bd3c316176683aa05fd6
goontor <claverie.pjr@gmail.com>
2023-09-19 16:37:13 +0800

    Chapter 8

--- Intro/intro.c
+++ Intro/intro.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
+#include <stdbool.h>
+
 int main() {
-    printf("\U0001F600");
+
+    int x = 200;
+    float y = 200.3f;
+    double z = 523.6;
+    char c = 'A';
+    char str[]= "Hello everyone";
+    bool b = true;
+
+    printf("x = %i \n", x);
+    printf("y = %f \n", y);
+    printf("z = %d \n", z);
+    printf("c = %c \n", c);
+    printf("str = %c \n", str);
+    printf("b = %i\n", b);
     return 0;
 }
